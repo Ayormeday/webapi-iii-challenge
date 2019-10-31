3f57e95988fac9841a3b080fd27e2d5c9a7a426e
Ayomide Jones <ayormeday@gmail.com>
2019-10-31 01:59:06 +0100

    postRouter added to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const express = require('helmet');
 const userRouter = require('./users/userRouter.js');
+const postRouter = require("./posts/postRouter.js");
 const app = express();
 
 app.use(helmet());
@@ -8,6 +9,7 @@ app.use(logger())
 app.use(express.json())
 
 app.use("/api/users", userRouter);
+app.use("/api/posts" postRouter);
 
 app.get("/", (req, res) => {
     res.send(`HELLO`);
