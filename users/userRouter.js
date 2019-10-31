9a65b2ad00416a2cea1f9c8badcaba19da8adb5a
Ayomide Jones <ayormeday@gmail.com>
2019-10-31 01:57:40 +0100

    userRouter completed

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const db = require("./userDb.js");
+const posts = require('../posts/postDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
+    db.insert({ name: req.body.name }).then(user => {
+      res.status(200).json(user);
+    }).catch(next);
+  });
+  
+  router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
+    posts.insert({ text: req.body.text, user_id: req.user.id }).then(post => {
+      res.status(200).json(post);
+    }).catch(next);
+  });
 
-});
-
-router.post('/:id/posts', (req, res) => {
-
-});
-
-router.get('/', (req, res) => {
-    db.find(req.query)
-    .then (userDb => {
-        res.status(200).json(userDb);
-    })
-    .catch(error => {
-        res.status(500).json({
-            message: `Error retrieving the user db`
-        })
-    })
-});
+  router.get('/', (req, res, next) => {
+    db.get().then(users => {
+      let usersPostsPromises = users.map(user => Users.getUserPosts(user.id));
+      Promise.all(usersPostsPromises).then(usersPostsLists => {
+        const usersWithPosts = users.map((user, i) => ({ ...user, posts: usersPostsLists[i] }));
+        res.status(200).json(usersWithPosts);
+      }).catch(next)
+    }).catch(next);
+  });
+  
 
 router.get('/:id', validateUserId, (req, res) => {
     res.status(200).json(req.user);
@@ -29,20 +33,20 @@ router.get('/:id', validateUserId, (req, res) => {
 });
 
 router.get('/:id/posts', validateUserId, (req, res, next) => {
-    Users.getUserPosts(req.user.id).then(posts => {
+    db.getUserPosts(req.user.id).then(posts => {
       res.status(200).json(posts);
     }).catch(next);
   });
 
   router.delete('/:id', validateUserId, (req, res, next) => {
-    Users.delete(req.user.id).then(deleted => {
+    db.delete(req.user.id).then(deleted => {
       res.status(200).json(req.user);
     }).catch(next);
   });
 
   router.put('/:id', validateUserId, validateUser, (req, res, next) => {
     const { name } = req.body;
-    Users.update(req.user.id, { name }).then(updated => {
+    db.update(req.user.id, { name }).then(updated => {
       res.status(200).json({ ...req.user, name });
     }).catch(next);
   });
@@ -94,5 +98,14 @@ function validatePost(req, res, next) {
         res.status(400).json({ message: "missing post data" });
       }
 };
+
+router.use((error, req, res, next) => {
+    res.status(500).json({
+      file: 'userRouter',
+      method: req.method,
+      url: req.url,
+      message: error.message
+    });
+  });
 
 module.exports = router;
